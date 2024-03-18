6ef078259eeb06cc399ffd44f0743749eb312fbc
joaobug <joaoenfrance@gmail.com>
2024-03-18 11:06:40 -0300

    Initial Commit

---
+++ LearnFrench/LearnFrenchApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnFrenchApp.swift
+//  LearnFrench
+//
+//  Created by Joao Leal on 3/18/24.
+//
+
+import SwiftUI
+
+@main
+struct LearnFrenchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
